8e50b2bc4067dad6b1e86abf8f051a009dbac505
pariwesh06 <pariweshg@gmail.com>
2018-10-15 12:05:18 +0530

    Add files via upload

---
+++ vue/Userform.vue
@@ -0,0 +1,44 @@
+<script>
+function foo() {
+  console.log("fooo");
+}
+const axios = require('axios');
+const baseUrl = 'https://itunes.apple.com/search?term=kishor';
+
+export default {
+  name: "userform",
+  data() {
+    return {
+      user: { name: "Ram", age: 0 },
+      users: []
+    };
+  },
+  methods: {
+    deleteUser(selectedIndex) {
+      console.log(event);
+      this.users.splice(selectedIndex, 1);
+    },
+    search(event) {
+      const promise = axios.get(baseUrl);
+      console.log(promise);
+      // this.users.push(Object.assign({}, this.user));
+    }
+  }
+};
+</script>
+
+<template>
+    <div>
+     <div>this is a component</div>
+        <input v-model="user.name"/>
+        <input v-model="user.age"/>
+        <button  v-on:click='search'>search</button>
+        <ol>
+          <li v-for="(user, index) in users">{{user.name}}, {{user.age}}
+            <button v-on:click='deleteUser( index)'>X</button>
+          </li>
+        </ol>
+    </div>
+</template>
+
+
